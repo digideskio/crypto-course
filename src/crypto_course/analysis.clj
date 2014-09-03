90cb92a3aa3327176473d33c53fdcd1655940134
emiln <emil@nauerby.com>
2014-09-03 23:14:01 +0200

    Implement index of coincidence

--- src/crypto_course/analysis.clj
+++ src/crypto_course/analysis.clj
@@ -12,3 +12,15 @@
   "Find every sequence of length n that appears more than once in coll."
   [n coll]
   (take-while #(> (val %) 1) (sorted-frequencies (partition n 1 coll))))
+
+(defn index-of-coincidence
+  "Returns the index of coincidence, which is the probability that two elements
+  - picked randomly from a set with frequencies as in freqs - are identical.
+  
+  The index of coincidence is expected to be ~0.065 for English text and ~0.038
+  for completely uniformly distributed letters."
+  [freqs]
+  (let [occur (vals freqs)
+        sum   (reduce + occur)
+        xform (map #(* % (dec %)))]
+    (/ (transduce xform + 0 occur) (* sum (dec sum)))))
